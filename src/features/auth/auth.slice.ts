3d099aa86dda1ab02032ef49c7f390ffd8559223
Hà Minh Long <longhm@i.lisod.vn>
2024-12-24 16:33:19 +0700

    Add docker config to project

--- src/features/auth/auth.slice.ts
+++ src/features/auth/auth.slice.ts
@@ -1,6 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { LoginApiResponse } from "../../api/auth";
-import { TypeUser } from "../../api/user";
 
 const initialState: LoginApiResponse["data"] = {};
 
--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const LoginPage = () => {
-  return <div className="text-[#f00]">LoginPage</div>;
+  return <div className="text-[#f00]">Login Page</div>;
 };
 
 export default LoginPage;
